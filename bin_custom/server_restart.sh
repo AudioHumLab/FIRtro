b7262e58d0b0fa62a19b48f85a2246df3e9cf13b
Rsantct <rsantct@gmail.com>
2017-04-01 11:29:12 +0200

    Update server_restart.sh

--- bin_custom/server_restart.sh
+++ bin_custom/server_restart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# matamos el server del FIRtro
 pkill -f server.py
-sleep .5
+
+# OjO hay veces que se queda algún proceso escuchando en 9999/tcp,
+# por ejemplo alsa_in ¿!?, hay que matarlo para liberar el puerto:
+fuser -k 9999/tcp
+
+# rearrancamos el server del FIRtro (9999/tcp)
 /home/firtro/bin/server.py &
+
