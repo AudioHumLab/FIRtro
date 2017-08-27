5c3405707fcf43c721c8008e93709eb5b4ddb83f
Rsantct <rsantct@gmail.com>
2017-08-27 11:45:36 +0200

    bug en la copia
    issue#21: la copia de los ocultos (.mpdconf etc) copiaba otras cosas accidentalmente

--- install_BETA/actualizaFIRtro.sh
+++ install_BETA/actualizaFIRtro.sh
@@ -68,10 +68,10 @@ fi
 
 cd $destino
 
-#########################################################
-# Respaldos .LAST de lo que hubiera ya configurado
-#########################################################
-echo \(i\) Guardando respaldos \(*.LAST\) de archivos de configuración
+######################################################################
+# Preventivo: hacemos respaldos .LAST de lo que hubiera ya configurado
+######################################################################
+echo "(i) Guardando respaldos *.LAST de archivos de configuración"
 # carpeta RAIZ:
 cp .mpdconf                 .mpdconf.LAST
 cp .brutefir_defaults       .brutefir_defaults.LAST
@@ -100,51 +100,56 @@ cp www/config/config.ini    audio/www_config.ini.LAST # en audio/ pq www/ desapa
 #########################################################
 # Limpieza
 #########################################################
-echo \(i\) Borrando archivos antiguos de FIRtro
-rm CHANGES*
-rm LICENSE*
-rm README*
-rm WIP*
+echo "(i) Borrando archivos antiguos de FIRtro"
+rm -f CHANGES*
+rm -f LICENSE*
+rm -f README*
+rm -f WIP*
 rm -rf bin/ # -f porque pueden haber *.pyc protegidos
 rm -r doc/
 rm -r www/
 rm .brutefir_c*
 
-
 #########################################################
 # Copiamos lo nuevo
 #########################################################
-echo \(i\) Copiando archivos nuevos desde $origen
-cp -r $origen/*     $destino/
-# y los ocultos:
-cp -r $origen/.*    $destino/
-
+echo "(i) Copiando archivos nuevos desde $origen en $destino"
+cp -r $origen/*             $destino/
+# y los ocultos se deben explicitar para que se copien:
+cp $origen/.mpdconf         $destino/
+cp $origen/.brutefir*       $destino/
+cp -r $origen/.mplayer*     $destino/
 
 ########################################################################
-# Si se ha pedido conservar las configuraciones
+# Si se ha pedido conservar las configuraciones las restauramos:
 ########################################################################
 if [ $conservar ]; then
-    echo \(i\) Restaurando configuraciones de usuario
+    echo "(i) Restaurando configuraciones de usuario"
+
     # carpeta RAIZ:
-    echo ".mpdconf"
+    echo "    .mpdconf"
     mv .mpdconf.LAST                .mpdconf
-    echo ".brutefir_defaults"
+    echo "    .brutefir_defaults"
     mv .brutefir_defaults.LAST      .brutefir_defaults
+
     # carpeta MPLAYER
-    echo ".mplayer/config"
+    echo "    .mplayer/config"
     mv .mplayer/config.LAST         .mplayer/config
-    echo ".mplayer/channels.conf"
+    echo "    .mplayer/channels.conf"
     mv .mplayer/channels.conf.LAST  .mplayer/channels.conf
+
     # carpeta WWW
-    echo "www/config/config.ini"
+    echo "    www/config/config.ini"
     mv audio/www_config.ini.LAST    www/config/config.ini.LAST
     cp www/config/config.ini.LAST   www/config/config.ini
+
     # carpeta AUDIO:
     for file in audio/*LAST ; do
         nfile=${file%.LAST}         # elimina .LAST encontrado al final '%'
-        echo $nfile
+        echo "    "$nfile
         mv $file $nfile
     done
+
 ########################################################################
 # Si NO se ha pedido conservar las configuraciones, se sobreescriben:
 ########################################################################
@@ -160,7 +165,7 @@ fi
 #########################################################
 # restaurando FIFOS
 #########################################################
-echo \(i\) Creando fifos para mplayer:
+echo "(i) Creando fifos para mplayer"
 rm -f *fifo
 mkfifo tdt_fifo
 mkfifo cdda_fifo
@@ -168,7 +173,7 @@ mkfifo cdda_fifo
 #########################################################
 # restaurando brutefir_convolver
 #########################################################
-echo \(i\) Un primer arranque de Brutefir para que genere archivos internos
+echo "(i) Un primer arranque de Brutefir para que genere archivos internos"
 brutefir
 
 #########################################################
@@ -182,5 +187,5 @@ chmod 666 www/config/config*
 # FIN
 #########################################################
 echo
-echo \(i\) Hecho. Para probar la configuración de prueba de FIRtro ejecutar el comando:
+echo "(i) Hecho. Para probar la configuración de prueba de FIRtro ejecutar el comando:"
 echo "    initfirtro.py"
