0454432232ac6f77a6ed23cb1b81b8799b4afaed
Rsantct <rsantct@gmail.com>
2017-08-29 22:30:10 +0200

    ayudante para probar jackd

---
+++ bin_custom.example/config_jack_options.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# v1.0
+
+if [ ! $1 ]; then
+    echo ""
+    echo "    Script para ayudar a reconfigurar las opciones de jack en audio/config"
+    echo "    Ejemplo de uso:  config_jack_option.sh  -p1024 -n3"
+    echo ""
+    exit 0
+fi
+
+f=/home/firtro/audio/config
+viejaLinea=$(grep "^jack_options" $f)
+echo ""
+echo "actual: "$viejaLinea
+echo ""
+
+
+read -r -p "ATENCION: deseas continuar? [y/N] " tmp
+if [ "$tmp" != "y" ] && [ "$tmp" != "Y" ]; then
+    echo Bye.
+    exit 0
+fi
+
+# Leemos las opciones
+opciones=""
+for x in "$@"; do
+    opciones+=$x" "
+done
+nuevaLinea="jack_options = -R -dalsa "$opciones
+
+# 'c' es el comando de 'sed' que reemplaza lineas completas de un archivo 
+# que matchean con /loquetiendelante/, entonces las reemplaza con loquetienedetrás.
+sed -i -e "/^jack_options*/c$nuevaLinea"   $f
+
+echo ""
+echo "nuevo:  "$(grep "^jack_options" $f)
+echo ""
