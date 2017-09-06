8065936ba68432100f30d2d04642479fc3b72837
Rsantct <rsantct@gmail.com>
2017-09-06 19:51:37 +0200

    rev  y simplificado

--- bin/server_input.py
+++ bin/server_input.py
@@ -10,44 +10,39 @@
 # - Se suprimen los sleep. (!) OJO si aparece un "error de cambio de input"
 #   puede ser debido un escenario de CPU% muy cargada afectando
 #   a las operaciones jack.disconnect/connect. Para debug se usa logging.
+#   Es posible insertar delays en el alias jack_connect/disconnect de abajo.
 #
-# v2.1b:
-# - Revisión del uso de los canales de radio
 #
 # v2.1c
 # - Revisión del pausado opcional de fuentes para ahorro de CPU
 # - Rev de comentarios del código
 #
 # v2.1d
-# - Se recupera el uso de getstatus para leer un posible cambio de presintonía
 # - Se reordena el código para legibilidad
+# - Se deja de intervenir aquí en los players integrados (radio, mpd, etc),
+#   se recurre al nuevo módulo players_integrated
 
 # módulos genéricos
 from os import path as os_path, remove as os_remove
-from subprocess import Popen
 from sys import path as sys_path
 from time import sleep
-from ConfigParser import ConfigParser
 import jack
 
+# módulos de FIRtro
 HOME = os_path.expanduser("~")
 sys_path.append(HOME + "/bin")
-
-# módulos del FIRtro
 from getconfig import *
-from basepaths import status_filename
-
-# FIRtro2: gestiona sound cards adicionales resampleadas en jack
-import soundcards as sc
-
-# FIRtro2: para reconectar la radio debido a mplayer -ao jack:mplayer:noconnect
-import radio_channel
 
 # FIRtro2: puertos de monitores de la señal (los convertimos a lista)
 ext_monitor_ports = jack_external_monitors.split()
 int_monitor_ports = jack_internal_monitors.split()
 
-# Para el DEBUG de excepciones
+# FIRtro2: gestiona sound cards adicionales resampleadas en jack
+import soundcards as sc
+# FIRtro2: gestiona los players integrados en un módulo separado
+import players_integrated as players
+
+# LOGGING para el DEBUG de excepciones
 # https://julien.danjou.info/blog/2016/python-exceptions-guide
 # https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
 import logging
@@ -56,7 +51,7 @@ if os_path.isfile(logFile):
     os_remove(logFile)
 logging.basicConfig(filename=logFile, level=logging.ERROR)
 
-# Alias con temporización experimental para operaciones jack.con/disconnect
+# Alias con retardos experimentales (normalmente no usados) para operaciones jack con/disconnect
 def jack_connect(p1, p2):
     jack.connect(p1, p2)
     #sleep(.1)
@@ -64,42 +59,9 @@ def jack_disconnect(p1, p2):
     jack.disconnect(p1, p2)
     #sleep(.2)
 
-# Función auxiliar para pausado opcional de fuentes para ahorro de CPU
-def pausas_opcionales(input_name):
-    # Pausa opcional de MPD
-    if load_mpd and pause_mpd:
-        if 'mpd' not in input_name.lower():
-            print "(server_input) Pausando MPD."
-            Popen("mpc pause > /dev/null 2>&1 &", shell=True)
-        else:
-            print "(server_input) Reanudando MPD."
-            Popen("mpc play > /dev/null 2>&1 &", shell=True)
-    # Pausa opcional de MOPIDY
-    if load_mopidy and pause_mopidy:
-        if 'mopidy' not in input_name.lower():
-            print "(server_input) Pausando MOPIDY."
-            Popen("mpc -p 7700 pause > /dev/null 2>&1 &", shell=True)
-        else:
-            print "(server_input) Reanudando MOPIDY."
-            Popen("mpc -p 7700 play > /dev/null 2>&1 &", shell=True)
-    # Pausa opcional de MPLAYER-TDT
-    if load_mplayer_tdt and pause_mplayer_tdt:
-        if 'tdt' not in input_name.lower():
-            print "(server_input) Parando  MPLAYER."
-            Popen("echo stop > " + HOME + "/tdt_fifo", shell=True)
-        else:
-            # releemos el estado por si se hubiera modificado la presintonia (v2.1b)
-            status.readfp(statusfile)
-            # recuperamos la presintonia
-            print "(serve_input) Resintonizando TDT presintonía: " + radio
-            if radio_channel.select_preset(radio):
-                print "(serve_input) Resintonizando TDT presintonía: " + radio
-            else:
-                print "(serve_input) ERROR resintonizando TDT presintonía: " + radio
-
 def desconecta_fuentes_de(out_ports):
     """ Desconectamos todos los clientes de la entrada del FIRtro y de los monitores
-    """    
+    """
     sources_L_firtro = jack.get_connections(out_ports[0])
     sources_R_firtro = jack.get_connections(out_ports[1])
     for source in sources_L_firtro:
@@ -107,7 +69,8 @@ def desconecta_fuentes_de(out_ports):
     for source in sources_R_firtro:
         jack_disconnect(source, out_ports[1])
 
-    try: #los monitores son opcionales
+    # Los monitores son opcionales
+    try:
         if ext_monitor_ports:
             sources_L_extMon = jack.get_connections(ext_monitor_ports[0])
             sources_R_extMon = jack.get_connections(ext_monitor_ports[1])
@@ -120,37 +83,37 @@ def desconecta_fuentes_de(out_ports):
     except:
         logging.exception("error en desconexion de monitores")
 
-###########################################################################################
-# Funcion original de FIRtro1 levemente modificada para los puertos de monitoreo opcionales
-###########################################################################################
+# Funcion original de FIRtro1 levemente modificada con
+# puertos de monitoreo y resincronización de posibles tarjetas externas
 def change_input(input_name, in_ports, out_ports, resampled="no"):
 
-    # 'in_ports':   Lista [L,R] de los puertos capture de jack de la input elegida
+    # 'in_ports':   Lista [L,R] de los puertos capture de jack de la fuente elegida
     # 'out_ports':  Lista de los puertos de la variable 'firtro_ports' que se resuelve
     #               en server_process en función de si se usa Brutefir/Ecasound
 
-    # Aquí evaluamos si la entrada requiere resampling por estar en una tarjeta adicional
+    # FIRtro2: evaluamos si la entrada requiere resampling por estar en una tarjeta adicional
     if resampled <> "no":
         sc.external_card_resync(in_ports, resampled)
 
-    # Pausado opcional de fuentes para ahorro de CPU
-    pausas_opcionales(input_name)
-
-    ####  cuerpo principal CASI como el original de FIRtro1:
+    # CONMUTADOR, como el original de FIRtro1.
     try:
         jack.attach("tmp")
-        
-        # primero desconectamos todo lo que hubiera en la entrada del firtro y monitores
+
+        # Desconectamos todo lo que hubiera en la entrada de FIRtro (y en los posibles monitores):
         desconecta_fuentes_de(out_ports)
 
-        # Y ahora conectamos la entrada deseada a al FIRtro y a los monitores
+        # Posibles PAUSAS opcionales en los PLAYERS INTEGRADOS (ahorro de %CPU):
+        players.manage_pauses(input_name)
+
+        # Y ahora conectamos la entrada deseada a FIRtro y a los monitores:
         for i in range(len(in_ports)):
+            # Entradas a FIRtro
             try:
                 jack_connect(in_ports[i], out_ports[i])
             except:
                 logging.exception("error conectando " + in_ports[i] + " <> " + out_ports[i])
 
-            # Los monitores opcionales
+            # Los monitores opcionales:
             try:
                 if ext_monitor_ports:
                     jack_connect(in_ports[i], ext_monitor_ports[i])
