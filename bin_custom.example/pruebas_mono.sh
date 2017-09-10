4d689987789b739ec8d40a0cdeb69fe66871d23a
Rsantct <rsantct@gmail.com>
2017-09-10 21:27:19 +0200

    Update pruebas_mono.sh

--- bin_custom.example/pruebas_mono.sh
+++ bin_custom.example/pruebas_mono.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+loadEcasound=$(grep load_ecasound /home/firtro/audio/config | cut -d"=" -f2)
+if [ $loadEcasound == True ]; then
+    pfirtro="ecasound"
+else
+    pfirtro="brutefir"
+fi
+
 verEstado () {
+echo "attBfir =" $(ba|tail -n1)
 grep "level ="  /home/firtro/audio/status
 grep "input ="  /home/firtro/audio/status
 grep "mono ="   /home/firtro/audio/status
-jack_conexiones.py ">"
+jack_conexiones.py ">" | grep " "$pfirtro | sort
 }
 
 echo " --- estado inicial:"
@@ -15,8 +23,10 @@ echo "--- cambio a estereo:"
 control mono off
 sleep 2
 verEstado
+echo ""
 
 echo "--- cambio a mono:"
 control mono on
 sleep 2
 verEstado
+echo ""
