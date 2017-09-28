3b0401c5eb0ade1c15adf9374702ee8610c44d92
Rsantct <rsantct@gmail.com>
2017-09-28 17:28:43 +0200

    Update getconfig.py

--- bin/getconfig.py
+++ bin/getconfig.py
@@ -26,6 +26,8 @@ enable_lcd =                config.getboolean('misc', 'enable_lcd')
 lcd_info_timeout =          config.getfloat('misc', 'lcd_info_timeout')
 lcd_layout =                config.get('misc', 'lcd_layout')
 enable_lcd_big =            config.getboolean('misc', 'enable_lcd_big')
+bigscroll_items =           config.get('misc', 'bigscroll_items').split()
+lcd_show_mute_prio =        config.get('misc', 'lcd_show_mute_prio')
 
 dummy_ports =               config.get('misc', 'dummy_ports')
 pause_players =             config.getboolean('misc', 'pause_players')
