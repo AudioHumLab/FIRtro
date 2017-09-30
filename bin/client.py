c7e178088781b1ef54a9fbb74606967b19f69383
Rsantct <rsantct@gmail.com>
2017-09-30 11:12:42 +0200

    +info prints

--- bin/client.py
+++ bin/client.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 # from __future__ import with_statement # This isn't required in Python 2.6
 
+# v1.0b
+# se etiquetan los 'print' para identificar la procedencia
+
 import socket
 import sys
 import os
@@ -15,50 +18,58 @@ def firtro_socket (data, quiet=False):
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        if not quiet: print "Connecting to",server, "port",str(port)+"..."
+        if not quiet: 
+            print "(client.py) Connecting to",server, "port",str(port)+"..."
         s.connect((server,port))
     except socket.gaierror, e:
-        print "Address-related error connecting to server: %s" % e
+        print "(client.py) Address-related error connecting to server: %s" % e
         sys.exit(-1)
     except socket.error, e:
-        print "Connection error: %s" % e
+        print "(client.py) Connection error: %s" % e
         sys.exit(-1)
+    if not quiet: 
+        print "(client.py) Connected"
 
-    if not quiet: print "Connected"
     try:
+
         # Si se pasa un parámetro, se envia al servidor y se cierra la conexión
         if data:
             s.send(data)
-            recibido=s.recv(4096)
-            #print recibido
+            recibido=s.recv(4096); # print "(client.py) recibido", recibido # DEBUG
             if data not in ("quit","close"):
-                #print "Received data:"
                 recibido = json.loads(recibido)
-                #if recibido(warnings): print recibido(warnings)
                 if len(recibido['warnings'])>0:
                     for value in recibido['warnings']:
-                        print "(client) Warning: " + value
-                #for key, value in recibido.iteritems(): print key, value
+                        print "(client.py) Warning: " + value
+                #for key, value in recibido.iteritems(): print key, value # DEBUG
                 s.send("close")
                 recibido=s.recv(4096)
+
+        # Si no se pasa parámetro, se abre una conexión interactiva
         else:
-            # Si no se pasa parámetro, se abre una conexión interactiva
-            print "close: closes the current connection"
-            print "quit: closes the current connection and the control script"
+            print "---- client.py - interactive session ----"
+            print "---- close: closes the current connection"
+            print "---- quit: closes the current connection and the control script"
             while True:
                 mensaje=raw_input(">>>")
                 s.send(mensaje)
-                recibido=s.recv(4096)
-                #print recibido
+                recibido=s.recv(4096); #print "(client.py)", recibido # DEBUG
                 if mensaje=="quit" or mensaje=="close":
                     print "Received data:", recibido
                     break
                 else:
-                    if not quiet: print "Received data:"
+                    if not quiet:
+                        print "Received data:"
                     recibido = json.loads(recibido)
-                    for key, value in recibido.iteritems(): print key, value
-    except: print "Unexpected error:", sys.exc_info()[0]
-    if not quiet: print "Closing connection..."
+                    for key, value in recibido.iteritems():
+                        print key, value
+
+    except:
+        print "(client.py) Unexpected error:", sys.exc_info()[0]
+
+    if not quiet:
+        print "(client.py) Closing connection..."
+
     s.close()
 
 if __name__ == "__main__":
