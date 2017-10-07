a125f8249e82fc2eecc5eb842ae22ee87e9f0219
Rsantct <rsantct@gmail.com>
2017-10-08 01:27:05 +0200

    Update spotify2lcd.py

--- bin/spotify2lcd.py
+++ bin/spotify2lcd.py
@@ -3,6 +3,7 @@
 """
     Script que atiende eventos GLib de Spotify y los comunica al server LCDd
 """
+
 # spotify2lcd.py
 # v1.0
 # - comprobamos que gi es un repositorio Python oficial.
@@ -35,10 +36,8 @@ from gi.repository import Playerctl, GLib
 
 from sys import argv as sys_argv, path as sys_path
 
-# acceso a 'server_lcd_big.py' de /home/firtro/bin
-from os import path as os_path
-sys_path.append("/home/firtro/bin")
-import server_lcd_big as lcd
+# cliente comun para interactuar con un servidor LCDproc
+import client_lcd as lcd
 
 def spotify_LCD(artistAlbumTitle, speed=3):
     # aux para printar los 3 elementos de artistAlbumTitle en el LCD
@@ -46,7 +45,7 @@ def spotify_LCD(artistAlbumTitle, speed=3):
     artist, album, title = artistAlbumTitle
     speed = str(speed)
     # creamos la pantalla
-    lcd._create_screen("spotify_scr1", duration=10)
+    lcd.create_screen("spotify_scr1", duration=10)
     # comandos para la linea 1: widget de título
     w1_add = "widget_add spotify_scr1 w1 title"
     w1_set = "widget_set spotify_scr1 w1 \ \ \ Spotify\ \ \ \ "
@@ -58,22 +57,20 @@ def spotify_LCD(artistAlbumTitle, speed=3):
     w3_set = "widget_set spotify_scr1 w3 1 3 20 3 m " + speed + " " + album.replace(" ", "\ ")
     w4_set = "widget_set spotify_scr1 w4 1 4 20 4 m " + speed + " " + title.replace(" ", "\ ")
     # lanzamiento de comandos al server LCDd
-    lcd.lcd_cmd_s(w1_add)
-    lcd.lcd_cmd_s(w2_add)
-    lcd.lcd_cmd_s(w3_add)
-    lcd.lcd_cmd_s(w4_add)
-    lcd.lcd_cmd_s(w1_set)
-    lcd.lcd_cmd_s(w2_set)
-    lcd.lcd_cmd_s(w3_set)
-    lcd.lcd_cmd_s(w4_set)
-
+    lcd.cmd_s(w1_add)
+    lcd.cmd_s(w2_add)
+    lcd.cmd_s(w3_add)
+    lcd.cmd_s(w4_add)
+    lcd.cmd_s(w1_set)
+    lcd.cmd_s(w2_set)
+    lcd.cmd_s(w3_set)
+    lcd.cmd_s(w4_set)
 
 def on_metadata(player, e):
     # handler para cuando hay un cambio de metadata en Spotify
     if 'xesam:artist' in e.keys() and 'xesam:title' in e.keys():
-        print 'Now playing:'
         artist_title = '{artist} - {title}'.format(artist=e['xesam:artist'][0], title=e['xesam:title'])
-        print artist_title
+        #print 'Spotify playing: ' + artist_title
 
         artist = e['xesam:artist'][0]
         album  = e['xesam:album']
@@ -83,22 +80,26 @@ def on_metadata(player, e):
 
 def on_play(player):
     # handler para cuando se inicia la reproducción en Spotify
-    print 'Playing at volume {}'.format(player.props.volume)
-
+    #print 'Spotify playing at volume {}'.format(player.props.volume)
+    pass
+    
 def on_pause(player):
     # handler para cuando se pausa Spotify
-    print 'Paused the song: {}'.format(player.get_title())
+    #print 'Paused the song: {}'.format(player.get_title())
     # borra del LDC la pantalla de Spotify si se pausa
-    lcd._delete_screen("spotify_scr1")
+    lcd.delete_screen("spotify_scr1")
 
 if __name__ == "__main__":
 
-    if len(sys_argv) > 1:
+    if len(sys_argv) == 2:
+        server = sys_argv[1]
+    else:
         print __doc__
         exit()
 
     # crea cliente conectado al server LCDd del sistema
-    lcd.crea_cliente("spotify_client")
+    lcd.crea_cliente("spotify_client", server)
+
     # crea una instancia de Playerctl, que es una interfaz dbus mpris para hablar con los player de un escritorio.
     player = Playerctl.Player(player_name='spotify')
 
@@ -115,3 +116,4 @@ if __name__ == "__main__":
     # BUCLE PRINCIPAL: wait for GLib events
     mainGlibLoop = GLib.MainLoop()
     mainGlibLoop.run()
+
