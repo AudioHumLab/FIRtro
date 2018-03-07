662fab2bba9f1a2db46c79380d2a0e8788d11168
Rsantct <rsantct@gmail.com>
2018-03-07 20:18:40 +0100

    Update ver_tarjetas_audio.py

--- bin_custom.example/ver_tarjetas_audio.py
+++ bin_custom.example/ver_tarjetas_audio.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
+"""
+    Script para visualizar el estado de las tarjetas alsa.
+    Admite una string como filtro de tarjeta a listar.
+"""
+# cat /proc/asound/CODEC/pcm0p/sub0/hw_params
+# access: MMAP_INTERLEAVED
+# format: S16_LE
+# subformat: STD
+# channels: 2
+# rate: 48000 (48000/1)
+# period_size: 4096
+# buffer_size: 12288
 
 import os,sys
 
 filtro = ""
 if len(sys.argv) > 1:
     if "help" in sys.argv[1]:
-        print "\n Script para visualizar el estado de las tarjetas alsa."
-        print " Admite una string como filtro de tarjeta a listar.\n"
+        print __doc__
         exit()
     else:
         filtro = sys.argv[1]
@@ -22,6 +33,11 @@ for linea in conte:
         t_index = linea.split("[")[0].strip()
         t_name  = "["+linea.split("[")[1].split("]")[0] + "]"
         tarjetas.append([t_index, t_name])
+
+if tarjetas:
+    # cabecera
+    print " card                          access              format subformat ch   rate               period  buffer"
+    #      [CODEC          ] pcm0c sub0 ['MMAP_INTERLEAVED', 'S16_LE', 'STD', '2', '48000 (48000/1)', '4096', '12288']
 
 for tarjeta in tarjetas:
     t_dir = "/proc/asound/" + tarjeta[1].split("[")[1].split("]")[0].strip()
@@ -43,4 +59,3 @@ for tarjeta in tarjetas:
                         else:
                             lista_hw_simple.append(cosa.strip())
                     print tarjeta[1].rjust(12, "_"), pcm, sub, lista_hw_simple
-                    
