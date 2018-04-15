fdf778790952d1ace6f1fd081c1d2e57476961de
Rsantct <rsantct@gmail.com>
2018-04-15 12:38:38 +0200

    Update patch_poweron_input.sh

--- bin_custom.example/patch_poweron_input.sh
+++ bin_custom.example/patch_poweron_input.sh
@@ -2,19 +2,24 @@
 
 # Se pretende evitar que FIRtro arranque con una input no deseada.
 # Por ejemplo mi input 'apple_tv' es digital y si está apagado
-# notaremos ruido blanco intermitente al arrancar
+# notaremos ruido blanco intermitente al arrancar, 
+# ya que el resampler hacia Jack no sincroniza.
+#
 # Entonces podemos alterar la input de audio/status antes de
 # arrancar FIRtro, insertando una llamada a este script en /etc/rc.local
+# antes de la ejecución de initfirtro.py
+
+# USO:  patch_poweron_input.sh  badInput  newInput
 
 # ---- CONFIGURACION ----
-badInput=spoti  # pattern
-goodInput=pulse
+badInput=$1
+newInput=$2
 
 # ---- MAIN ----
 statusfile='/home/firtro/audio/status'
 if [[ ! $(grep -i $badInput $statusfile) == '' ]]; then
     # insert la nueva linea, para append seria '/pattern/a nueva_linea '
-    sed -i '/input\ =/i input\ =\ '$goodInput  $statusfile
+    sed -i '/input\ =/i input\ =\ '$newInput  $statusfile
     # borramos la vieja '/pattern/d'
     sed -i '/'$badInput'/d'  $statusfile
 fi
