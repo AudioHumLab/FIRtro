57327c758d75601df037422f4fbedfa4c7add1f1
Rsantct <rsantct@gmail.com>
2018-08-08 21:51:22 +0200

    Update stopfirtro.py

--- bin/stopfirtro.py
+++ bin/stopfirtro.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
-#coding=utf-8
-# from __future__ import with_statement # This isn"t required in Python 2.6
+# -*- coding: utf-8 -*-
 """
+    'stoptfirtro.py' detiene el server y los módulos de FIRtro.
+
     Uso:
-    stopfirtro.py  [ audio | core | all* ]   (* por defecto)
-    audio: Jack, Brutefir, Ecasound.
-    core:  + Mpd y otros players, Netjack.
-    all:   + Server, Lirc.
+
+    stoptfirtro.py [ core | audio | +players | all ]   (por defecto 'all')
+
+    core o audio:   Jack, Brutefir, Ecasound.
+    +players:       + MPD y otros players
+    all:            + control por Lirc y algunos clients para display
 """
 # v2.0
-# reordenación del run_level
-# reordenación del código evitando líneas multisentencia
+#   Reordenación del run_level
+#   Reescritura del código evitando líneas multisentencia
+# v2.0b (revisión)
+# v2.0c (revisión)
 
 import client
 import time
@@ -27,85 +32,89 @@ fnull = open(os.devnull, 'w')
 
 def main(run_level):
 
-    if run_level in ["audio", "core", "all"]:
+    if run_level in ["all"]:
+        # lirc
+        if load_irexec:
+            Popen(["killall", irexec_path], stdout=fnull, stderr=fnull)
+        # controlserver
+        try:
+            client.firtro_socket("quit")
+            time.sleep(1)
+        except:
+            Popen (["pkill", "-9", "-f", control_path], stdout=fnull, stderr=fnull)
+            time.sleep(.5)
+        # client_mpd
+            Popen (["pkill", "-9", "-f", "client_mpd.py"], stdout=fnull, stderr=fnull)
+        # LCD_server
+        if load_LCD_server:
+            Popen (["pkill", "-9", "-f", LCD_server_path], stdout=fnull, stderr=fnull)
+        # mpdlcd (MPD client for lcdproc)
+        if load_mpdlcd:
+            Popen (["pkill", "-9", "-f", mpdlcd_path], stdout=fnull, stderr=fnull)
+        # INFOFIFO_server
+        if load_INFOFIFO_server:
+            Popen (["pkill", "-9", "-f", INFOFIFO_server_path], stdout=fnull, stderr=fnull)
+        # spotifymonitor
+        if load_spotifymonitor:
+            Popen (["pkill", "-9", "-f", spotifymonitor_path], stdout=fnull, stderr=fnull)
+        # mpdmonitor
+        if load_mpdmonitor:
+            Popen (["pkill", "-9", "-f", mpdmonitor_path], stdout=fnull, stderr=fnull)
+        # spotifymonitor
+        if load_spotifymonitor:
+            Popen (["pkill", "-9", "-f", spotifymonitor_path], stdout=fnull, stderr=fnull)
 
-        if run_level in ["all"]:
-            # lirc
-            if load_irexec:
-                Popen(["killall", irexec_path], stdout=fnull, stderr=fnull)
-            # controlserver
-            try:
-                client.firtro_socket("quit")
-                time.sleep(1)
-            except:
-                Popen (["pkill", "-9", "-f", control_path], stdout=fnull, stderr=fnull)
-                time.sleep(.5)
-            # client_mpd
-                Popen (["pkill", "-9", "-f", "client_mpd.py"], stdout=fnull, stderr=fnull)
+    if run_level in ["+players", "all"]:
+        # mpd
+        if load_mpd:
+            Popen ("killall mpd", shell=True)
+        # mplayer
+        if load_mplayer_cdda or load_mplayer_tdt:
+            Popen (["killall", mplayer_path], stdout=fnull, stderr=fnull)
+        # mopidy
+        if load_mopidy:
+            Popen (["killall", mopidy_path], stdout=fnull, stderr=fnull)
+        # squeezeslave
+        if load_squeezeslave:
+            Popen (["killall", squeezeslave_path], stdout=fnull, stderr=fnull)
+        # shairport
+        if load_shairport:
+            Popen (["killall", shairport_path], stdout=fnull, stderr=fnull)
 
-        if run_level in ["core", "all"]:
-            # LCD_server
-            if load_LCD_server:
-                Popen (["pkill", "-9", "-f", LCD_server_path], stdout=fnull, stderr=fnull)
-            # INFOFIFO_server
-            if load_INFOFIFO_server:
-                Popen (["pkill", "-9", "-f", INFOFIFO_server_path], stdout=fnull, stderr=fnull)
-            # spotifymonitor
-            if load_spotifymonitor:
-                Popen (["pkill", "-9", "-f", spotifymonitor_path], stdout=fnull, stderr=fnull)
-            # mpdmonitor
-            if load_mpdmonitor:
-                Popen (["pkill", "-9", "-f", mpdmonitor_path], stdout=fnull, stderr=fnull)
-            # mpdlcd (MPD client for lcdproc)
-            if load_mpdlcd:
-                Popen (["pkill", "-9", "-f", mpdlcd_path], stdout=fnull, stderr=fnull)
-            # spotifymonitor
-            if load_spotifymonitor:
-                Popen (["pkill", "-9", "-f", spotifymonitor_path], stdout=fnull, stderr=fnull)
-            # mpd
-            if load_mpd:
-                Popen ("killall mpd", shell=True)
-            # mopidy
-            if load_mopidy:
-                Popen (["killall", mopidy_path], stdout=fnull, stderr=fnull)
-            # squeezeslave
-            if load_squeezeslave:
-                Popen (["killall", squeezeslave_path], stdout=fnull, stderr=fnull)
-            # shairport
-            if load_shairport:
-                Popen (["killall", shairport_path], stdout=fnull, stderr=fnull)
-            # jacktrip
-            if load_jacktrip:
-                Popen (["killall", jacktrip_path], stdout=fnull, stderr=fnull)
-            # netjack
-            if load_netjack:
-                Popen (["killall", netjack_path], stdout=fnull, stderr=fnull)
-            # zita-njbridge
-            if load_zita_j2n:
-                Popen (["killall", zita_j2n_path], stdout=fnull, stderr=fnull)
-            if load_zita_n2j:
-                Popen (["killall", zita_n2j_path], stdout=fnull, stderr=fnull)
-            # mplayer
-            if load_mplayer_cdda or load_mplayer_tdt:
-                Popen (["killall", mplayer_path], stdout=fnull, stderr=fnull)
+    # El core de audio siempre se reinicia:
+    # jacktrip
+    if load_jacktrip:
+        Popen (["killall", jacktrip_path], stdout=fnull, stderr=fnull)
+    # netjack
+    if load_netjack:
+        Popen (["killall", netjack_path], stdout=fnull, stderr=fnull)
+    # zita-njbridge
+    if load_zita_j2n:
+        Popen (["killall", zita_j2n_path], stdout=fnull, stderr=fnull)
+    if load_zita_n2j:
+        Popen (["killall", zita_n2j_path], stdout=fnull, stderr=fnull)
+    # ecasound
+    if load_ecasound:
+        Popen (["killall", "-KILL", "ecasound"], stdout=fnull, stderr=fnull)
+    # brutefir
+    Popen (["killall", brutefir_path], stdout=fnull, stderr=fnull)
+    # jack
+    Popen (["killall", "jackd"], stdout=fnull, stderr=fnull)
 
-        # ecasound
-        if load_ecasound:
-            Popen (["killall", "-KILL", "ecasound"], stdout=fnull, stderr=fnull)
-        # brutefir
-        Popen (["killall", brutefir_path], stdout=fnull, stderr=fnull)
-        # jack
-        Popen (["killall", "jackd"], stdout=fnull, stderr=fnull)
+    time.sleep(1)
 
-        time.sleep(1)
-
-    else:
-        print __doc__
 
 if __name__ == "__main__":
+
+    run_level = "all"
     if sys.argv[1:]:
         run_level = sys.argv[1].lower()
+
+    if run_level == "core": # legacy compatible
+        run_level = "audio"
+
+    if run_level in ["audio", "+players", "all"]:
+        print "(stopfirtro) deteniendo: " + run_level
+        main(run_level)
     else:
-        run_level = "all"
-    print "(stopfirtro) deteniendo: " + run_level
-    main(run_level)
+        print __doc__
