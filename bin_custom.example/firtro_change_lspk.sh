3b0084f5c882da976f8952b4e0cd9e2b36b8991e
Rsantct <rsantct@gmail.com>
2018-08-08 21:35:21 +0200

    Update firtro_change_lspk.sh

--- bin_custom.example/firtro_change_lspk.sh
+++ bin_custom.example/firtro_change_lspk.sh
@@ -2,39 +2,49 @@
 
 # Cutrescript para relanzar FIRtro con otro altavoz
 
-# 1. Comprobammos que disponemos de la carpeta de configuración del altavoz
+# v0.2
+#  - Matamos el server para que no interfiera
+
+# 0. Comprobammos que disponemos de la carpeta de configuración del altavoz
 altavoz=$1
 if [ ! $altavoz ]; then
     echo "indicar altavoz"
     exit 0
 fi
 if [ -d /home/firtro/lspk/$altavoz ]; then
-    echo "(i) Se reiniciará FIRtro con altavoz '"$altavoz"' ... ..."
+    echo "(firtro_change_lspk) Se reiniciará FIRtro con el altavoz: "$altavoz
 else
     echo "NO existe "$altavoz
     exit 0
 fi
 
-# Para reemplazar una linea que cumpla una pattern con otra linea:
-# sed -e ‘/pattern_de_linea_a_reeemplazar/c\nueva_linea’   /path/to/file
+# 1. Matamos el server para que no interfiera:
+echo "(firtro_change_lspk) Cerrando server.py"
+pkill -f "python /home/firtro/bin/server.py"
+# OjO hay veces que se queda algún proceso escuchando en 9999/tcp,
+# por ejemplo alsa_in ¿!?, hay que matarlo para liberar el puerto:
+fuser -kv 9999/tcp
 
-# 2. Si hay algún preset previsto en la carpeta del altavoz,
-#    lo cargamos en audio/status para que se use en al arranque:
+# nota: para reemplazar en un archivo una linea que cumpla una pattern con otra linea:
+# sed -e '/pattern_de_linea_a_reeemplazar/c\nueva_linea'   /path/to/file
+
+# 2. Configuramos el altavoz solicitado en audio/config
+sed  -i.bak -e '/loudspeaker\ \=/c\loudspeaker\ \=\ '$altavoz /home/firtro/audio/config
+echo "(firtro_change_lspk) audio/config  ---> "$(grep ^loudspeaker /home/firtro/audio/config)
+
+# 3. Si hay algún preset previsto en la carpeta del altavoz,
+#    lo cargamos en audio/status para que se use en el arranque:
 tmp=$(grep "\[" /home/firtro/lspk/$altavoz/presets.ini \
       | grep -v ";" | grep -v "#" | sed 's/\[//g' | sed 's/\]//g')
 preset=$(echo $tmp | cut -d" " -f1)
 if [ $preset ]; then
-    echo "(i) Configurando preset '"$preset"' en audio/status"
-    sed  -i.bak -e '/preset/c\preset\ \=\ '$preset /home/firtro/audio/status
+    sed  -i -e '/preset/c\preset\ \=\ '$preset /home/firtro/audio/status
 else
     # Si no lo hubiera, borramos el preset de audio/status:
-    echo "(i) Borrando preset de audio/status"
-    sed  -i.bak -e '/preset/c\preset\ \=\ ' /home/firtro/audio/status
+    sed  -i -e '/preset/c\preset\ \=\ ' /home/firtro/audio/status
 fi
+echo "(firtro_change_lspk) audio/status  ---> "$(grep preset /home/firtro/audio/status)
 
-# 3. Configuramos el altavoz solicitado en audio/config
-sed  -i.bak -e '/loudspeaker\ \=/c\loudspeaker\ \=\ '$altavoz /home/firtro/audio/config
-
-# 4. Reiniciamos FIRtro
-echo "(i) REINICIANDO FIRtro:"
-initfirtro.py
+# 4. Reiniciamos los módulos de audio de FIRtro
+echo "(firtro_change_lspk) REINICIANDO los módulos de audio de FIRtro:"
+initfirtro.py audio
