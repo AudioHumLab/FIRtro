3558c71e0148a1309f12018774c87db97803a51a
Rsantct <rsantct@gmail.com>
2018-09-24 14:53:33 +0200

    Add files via upload

--- bin/do_brutefir_config.py
+++ bin/do_brutefir_config.py
@@ -3,15 +3,12 @@
 
 ###################################################################################################
 #################################   DISCLAIMER ACHTUNG   ##########################################
-###################################################################################################
-#                ESTE SCRIPT ES MUY RUDIMENTARIO HABRIA QUE REESCRIBIRLO :-/
-#                INCLUYENDO EL SCANEO DE PCMs Y TAL ...
-###################################################################################################
-###################################################################################################
+#                ESTE SCRIPT ES MUY RUDIMENTARIO QUEDA PENDIENTE
+#                REESCRIBIRLO INCLUYENDO EL SCANEO DE PCMs Y TAL :-/
 ###################################################################################################
 
 # v1.0: se permite argumento "-w" para escribir la salida en el archivo brutefir_config
-# v1.1: incorpora un coeff de xover "dirac pulse" para poder cargarlo en una vía full range"
+# v1.1: incorpora un coeff de xover "dirac pulse" para poder cargarlo en una vía full range alineada con otras.
 # v1.2: se permiten nuevos argumentos:
 #     - un archivo brutefir.ini alternativo
 #     - una carpeta de altavoz distinta de la configurada en el sistema en audio/config
@@ -21,55 +18,84 @@
 # v1.2b:
 #   - Se adaptan las etapas de filtrado eq y drc para podeer hacer MONO: drc recibe ambos canales de eq.
 # v1.2c:
-#   - Bug en la lectura de la carpeta de altavoz de referencia si se pasa como argumento
-#   - Bug se leia <audio_folder>/filters.ini antes del escaneo
-#   - Se renombra a do_brutefir_config.py por respeto
-#   - Se adapta al abandono de la ubicación de PCMs para DRC en subcarpetas de la de audio 
-
+#   - Bug se leia <audio_folder>/filters.ini antes del escaneo (ahora llamado filters.scan)
+#   - Ahora se debe indicar la carpeta del altavoz para el que se quiere construir brutefir_config.
+#       - La fs por defecto será 44100
+#       - Debe existir un archivo '~/lspk/ALTAVOZ/brutefir_config.ini'.
+#   - Se renombra a do_brutefir_config.py por claridad.
+#   - Se adapta al abandono de la ubicación de PCMs para DRC en subcarpetas de la de audio
+#   - Se adapta al cambio de nombre filters.ini --> filters.scan que refleja mejor la naturaleza de este archivo.
+#   - Se mejora el algoritmo de elección de filtros pcm para construir la esctructura de filtrado.
+#
 """
+Construye un archivo 'brutefir_config' para FIRtro.
+
 Uso:
-    do_brutefir_config.py opciones
+    do_brutefir_config.py lskp/ALTAVOZ  [FS] [-w | -h]
 
-    opciones:
-        /path/to/'brutefir.ini'file   (obligatorio)
-        /path/to/lskp/altavoz         (por defecto el configurado en ~/audio/config)
-        -w                            escribe la salida en el archivo brutefir_config
-        -h --help                     muestra esta ayuda
+        FS          fs por defecto 44100
+        -w          escribe la salida en lspk/ALTAVOZ/FS/brutefir_config
+        -h --help   muestra esta ayuda
 
-Este script de usuario construye un archivo "brutefir_config" en función de:
+El archivo 'brutefir_config' se construye en función de:
 
-1) Un archivo de configuración básica ~/audio/brutefir.ini que especifica parámetros generales:
-    - la longitud y particionado de filtros, si se usa dither, etc
-    - el mapeo de entradas y salidas en JACK, así como la polaridad,
+1) Un archivo de configuración 'lspk/ALTAVOZ/brutefir_config.ini' preparado
+   por el USUARIO que especifica los parámetros necesarios:
+
+    - La longitud y particionado de filtros, si se usa dither, etc
+
+    - El MAPEO de entradas y salidas en JACK, así como la polaridad,
       atenuación y delays de las salidas.
 
-2) La relación de pcms de filtrado que se encuentre en la carpeta del altavoz. Cada "xxxxxx.pcm"
-   debe acompañarse de un "xxxxxx.ini" que contiene la ganancia del filtro (a modo de metadato
-   en archivo paraleleo) para ser tenida en cuenta a la hora de declarar los coeff en brutefir_config.
+2) La relación de pcms de filtrado que el USUARIO haya depositado
+   en la carpeta de audio altavoz 'lspk/ALTAVOZ/FS'
 
-Este script hace uso del módulo scanfilters.py que genera el archivos auxiliar
-"~/lspk/altavoz/Fs/filters.ini", con la clasificación de filtros pcm encontrados
+Cada .pcm puede acompañarse de un .ini indicando la ganancia del FIR,
+entonces se incorporará en la declaración 'coeff' correspondiente.
+
+Este script hace uso del módulo 'scanfilters.py' que genera el archivo auxiliar
+'lspk/ALTAVOZ/FS/filters.scan', con la clasificación de filtros pcm encontrados
 en la carpeta del altavoz.
 
-En función de los nombres de archivos de filtro encontrados, se construirá una estructura de
-filtrado adecuada.
+Se construirá una estructura de filtrado adecuada en función de los NOMBRES de
+los archivos .PCM encontrados.
 
-Los nombres de los archivos de filtrado deben acomodarse a:
+En caso de encontrar filtros 'sw' para subwoofer se incluirá una etapa de filtrado
+a partir de la mezcla ponderada de los canales L y R.
 
-    aa-bb_nombre_descriptivo.pcm  (sin espacios)
+Se priorizan los filtros 'lp' para construir la cadena de filtrado,
+si no existieran se usarán los 'mp'.
 
-    siendo:
-    aa: 'mp' 'lp'
-    bb: 'hi' 'mi' 'lo' 'sw' 'fr'  (fr es indicativo de que se ecualiza una caja pasiva "full range")
+---------------    REGLAS DE NOMBRADO de archivos pcm:    ---------------
 
-En caso de encontrar filtros "sw" para subwoofer se incluirá una etapa de filtrado a partir
-de la mezcla ponderada de los canales L y R provinientes de la etapa de drc, la misma etapa
-que proporciona la señal a las etapas de filtrado de vías.
+Todos los .pcm admiten opcionalmente una cadena '_descriptivo' antes del sufijo '.pcm'
 
-NOTA: en caso de existir varios pcm para una misma vía, se asignará el primero listado
-en el archivo filters.ini que se menciona arriba.
-Si se desea cargar un pcm determinado, se debe de hacer un cambio al vuelo mediante el CLI de Brutefir
-o bien nombrar el archivo con un orden alfabético menor.
+- pcms de DRC
+
+    drc-N-C.pcm
+    drc-N-C_descriptivo.pcm
+
+    N: secuencial desde 1
+    C: canal 'L' o  'R' al que va destinado
+
+- pcms de VIAS ordinarias (separadas en cada canal)
+
+    aa-bb.pcm
+    aa-bb-C.pcm                 Opcional para dedicar el pcm a un CANAL
+    aa-bb_descriptivo.pcm
+    aa-bb-C_descriptivo.pcm
+
+    aa: 'lp' 'mp'
+    bb: 'hi' 'mi' 'lo' 'fr'     (fr indica "full range")
+    C:  'L' 'R'                 (opcional)
+
+- pcms de SUBWOOFER (recibirán la señal de todos los canales)
+
+    aa-sw-ID.pcm
+    aa-sw-ID_descriptivo.pcm
+
+    aa: 'lp' 'mp'
+    ID: identificador         (preferiblemente breve)
 
 """
 
@@ -82,7 +108,6 @@ import scanfilters
 HOME = os_path.expanduser("~")
 sys.path.append(HOME + "/bin")
 from basepaths import loudspeaker_folder
-from getconfig import loudspeaker
 from getstatus import fs
 
 def verificaEstructuraVias():
@@ -96,8 +121,10 @@ def verificaEstructuraVias():
     for viaINI in bf_ini.options("outputs"):
         if viaINI[:2] in tiposDeVias:
             if viaINI not in VIAS:
-                # por compatibilidad ponemos el sufijo del canal en mayúscula.
-                VIAS.append(viaINI[:-1] + viaINI[-1].upper())
+                # Compatibilidad con server_process: sufijo del canal en mayúscula.
+                if not viaINI[:2] == 'sw':
+                    viaINI = viaINI[:-1] + viaINI[-1].upper()
+                VIAS.append(viaINI)
         else:
             print 'nombre de via incorrecto:', viaINI
             return False, [viaINI], False
@@ -132,12 +159,11 @@ def hacer_CadenaConvolver(VIAS):
             print '    from_filters: "f_eq_L"//0, "f_eq_R"//1 ;'
 
         # Ahora debemos llevar la señal a las VIAs y a los SUBs si existieran
-        # tmp1 recoje las vias separadas por canal
-        tmp1 = [via for via in VIAS if '_' + canal in via]
-        tmp1 = [x[:-1]+x[-1].upper() for x in tmp1]
-        # tmp2 recoje los posibles subwoofers
-        tmp2 = [via for via in VIAS if 'sw' in via]
-        tmp2 = [x[:-1]+x[-1].upper() for x in tmp2]
+        # tmp1 recoje las vias separadas por *canal*
+        tmp1 = [ via for via in VIAS if '_' + canal in via ]
+        tmp1 = [ x[:-1]+x[-1].upper() for x in tmp1]
+        # tmp2 recoje los posibles *subwoofers*
+        tmp2 = [via for via in VIAS if via[:2]=='sw' ]
         print '    to_filters:   "f_' + '", "f_'.join(tmp1 + tmp2) + '";'
 
         # podemos usar el primer DRC:
@@ -149,41 +175,105 @@ def hacer_CadenaConvolver(VIAS):
 
     # XOVER filtering:
     print '\n# --- XOVER filtering:\n'
+
+    # 1. Agrupamos por canales
     for canal in CANALES:
-        for via in [x for x in VIAS if '_' + canal in x]:
-            gain, polarity, delay = bf_ini.get("outputs", via).split()[1:]
-            print 'filter "f_' + via + '" {'
+
+        # (A) RECORREMOS las VIAS declaradas en 'brutefir_config.ini', las de este canal.
+        # OjO 'via' en brutefir_config.ini es vv_ch o sea via+canal (abajo 'viacha')
+        #     Aquí vamos a centrarnos en un canal para hacer la cadena de xover ordenada
+        for via in [viacha[:2] for viacha in VIAS if canal == viacha[-1] ]:
+            gain, polarity, delay = bf_ini.get("outputs", via+'_'+canal).split()[1:]
+
+            # (B) RECORREMOS los coeffs listados en 'filters.scan'
+            #
+            #     Opcionalmente el .pcm podrá estar nombrado con el canal si es que se
+            #     ubiera diseñado un filtrado diferenciado en alguna vía, ejemplo:
+            #
+            #     [lp_xo]
+            #     c_lp-lo1 = ...../lp-lo_woofer.pcm
+            #     c_lp-hi1 = ...../lp-hi-L_tweeter.pcm
+            #     c_lp-hi2 = ...../lp-hi-R_tweeter.pcm
+            #     ... etc ...
+
+            # Priorizamos filtros lp
+            if filters_ini.options('lp_xo'):
+                inipha = 'lp_xo'
+            else:
+                inipha = 'mp_xo'
+
+            matched = ''
+            pcmPrioritario = False
+            for coeff in filters_ini.options(inipha):
+                pcm      = filters_ini.get(inipha, coeff).split("/")[-1]
+                pcmvia   = pcm[3:5]
+                pcmcan   = ''
+                if pcm[5:7] == '-L':
+                    pcmcan = 'L'
+                if pcm[5:7] == '-R':
+                    pcmcan = 'R'
+
+                if pcmvia == via:
+                    if pcmcan == canal:
+                        pcmPrioritario = True
+                        matched = coeff
+                    # Se prioriza un pcm dedicado a un canal aunque apareciera otro genérico
+                    elif not pcmcan and not pcmPrioritario:
+                            matched = coeff
+
+            print 'filter "f_' + via + "_" + canal + '" {'
             print '    from_filters: "f_drc_' + canal + '";'
-            print '    to_outputs:   "' + via + '"/' + gain + '/' + polarity + ';'
+            print '    to_outputs:   "' + via + '_' + canal + '"/' + gain + '/' + polarity + ';'
 
-            # busquemos el primer coeff disponible para la via en filters.ini
-            for option in filters_ini.options('lp_xo'):
-                if option[5:7] == via[:2]:  # ej: fr_l ---> fr
-                    break
-            print '    coeff:        "' + option + '";'
-
+            if matched:
+                print '    coeff:        "' + matched + '";'
+            else:
+                if via == 'fr':
+                    print '    coeff:        -1;'
+                else:
+                    print '    coeff:        "COEFF NO ENCONTRADO PARA ESTA VIA";'
             print '};'
 
     # SUBWOOFER filtering
-    if any(via for via in VIAS if 'sw' in via):
+    if any(via for via in VIAS if via[:3]=='sw_' ):
         print '\n# --- SUB filtering:\n'
 
-        for via in [x for x in VIAS if 'sw' in x]:
+        # (A) RECORREMOS las vias de subwoofer declaradas en 'brutefir_config.ini'
+        for via in [x for x in VIAS if x[:2] == 'sw']:
 
             gain, polarity, delay = bf_ini.get("outputs", via).split()[1:]
             mixAtt = '{:.2}'.format(-10 * np_log10(1.0/len(CANALES)))
+
+            # (B) RECORREMOS los coeffs listados en 'filters.scan'
+
+            # Priorizamos filtros lp
+            if filters_ini.options('lp_sw'):
+                inipha = 'lp_sw'
+            else:
+                inipha = 'mp_sw'
+
+            matched = ''
+            for coeff in filters_ini.options(inipha):
+                pcm     = filters_ini.get(inipha, coeff).split("/")[-1]
+                # ejemplos  via:     sw_amr     pcm:      lp-sw-amr_60Hz.pcm
+                #                               pcm:      lp-sw-amr.pcm
+                #                               pcmvia:   sw-amr
+                #           viaswID:    amr     pcmswID:     amr
+                pcmvia  = pcm [3:].replace('.pcm','').split("_")[0]
+                pcmswID = pcmvia.split("-")[-1]
+                viaswID = via.split("_")[-1]
+
+                if pcmswID == viaswID:
+                    matched = coeff
 
             print 'filter "f_' + via + '" {'
             tmp = '"f_drc_' + ('"/' + mixAtt + ', "f_drc_').join(CANALES) + '"/' + mixAtt +';'
             print '    from_filters: ' + tmp
             print '    to_outputs:   "' + via + '"/' + gain + '/' + polarity + ';'
-
-            # busquemos el primer coeff disponible para la via en filters.ini
-            for option in filters_ini.options('lp_sw'):
-                if option[5:7] == via[:2]:  # ej: fr_l ---> fr
-                    break
-            print '    coeff:        "' + option + '";'
-
+            if matched:
+                print '    coeff:        "' + matched + '";'
+            else:
+                print '    coeff:        "COEFF NO ENCONTRADO PARA ESTE SUBWOOFER";'
             print '};'
 
 def hacer_IO():
@@ -239,7 +329,7 @@ def hacer_IO():
     delaysMS= []
     for via in VIAS:
         delaysMS.append(bf_ini.get("outputs", via).split()[3])
-    delaysSamples = [x.replace(x, str(int(fs*float(x)/1000))) for x in delaysMS]
+    delaysSamples = [x.replace(x, str(int(int(fs)*float(x)/1000))) for x in delaysMS]
     print '    delay:    ' + ','.join(delaysSamples) + '; # \'samples\' that are equivalent in \'ms\' to ' \
           + ','.join(delaysMS)
 
@@ -267,9 +357,12 @@ def hacer_Coef_DRC():
     for coeff in filters_ini.options("drcs"):
         gain = filters_ini.get("drcs", coeff).split()[0]
         file = ' '.join(filters_ini.get("drcs", coeff).split()[1:])
-        # por compatibilidad mayuscula al final
-        tmp = coeff[:-1] + coeff[-1].upper()
-        print 'coeff "' + tmp + '" {'
+
+        # Compatibilidad con server_process: sufijo del canal en mayúscula.
+        if not coeff[:2] == 'sw':
+            coeff = coeff[:-1] + coeff[-1].upper()
+
+        print 'coeff "' + coeff + '" {'
         print '    filename:    "' + file + '";'
         print '    format:      "FLOAT_LE";  shared_mem:  false;'
         print '    attenuation: ' + gain + ';'
@@ -333,7 +426,11 @@ def lee_secc_gral(param):
     return bf_ini.get("general", param ).split(';')[0].split('#')[0].strip().lower()
 
 def hacer_cabecera():
-
+    print '# --------------------------------------------------'
+    print '#    GENERADO POR do_brutefir_config.py BASADO EN:'
+    print '#    ' + bf_ini_file
+    print '# --------------------------------------------------'
+    print
     print '# ------------------------------'
     print '# ------ GENERAL SETTINGS ------'
     print '# ------------------------------\n'
@@ -359,16 +456,19 @@ if __name__ == "__main__":
     bf_ini      = ConfigParser.ConfigParser()
     filters_ini = ConfigParser.ConfigParser()
     avisos      = []
+    loudspeaker = ''
+    fs          = '44100' # por defecto
 
-    # El archivo brutefir.ini es obligatorio como argumento.
-    bf_ini_file = ""
-
-    # La ubicación por defecto de la carpeta del altavoz se toma de ~/audio/config
-    audio_folder = loudspeaker_folder + loudspeaker + "/" + fs
-
-    # Si se pasan argumentos los atendemos
+    # Leemos argumentos:
     if sys.argv[1:]:
+
         args = sys.argv[1:]
+
+        # se pide ayuda
+        tmp = [ x for x in args if "-h" in x ]
+        if tmp:
+            print __doc__
+            sys.exit(0)
 
         # si se pide escritura de la salida
         if "-w" in args:
@@ -376,36 +476,34 @@ if __name__ == "__main__":
         else:
             salidaPorConsola = True
 
-        # archivo brutefir.ini
-        tmp = [x for x in args if ".ini" in x and "/" in x]
-        if tmp:
-            bf_ini_file = tmp[0]
-
-        # si se indica una carpeta de altavoz en particular
-        # OjO debe proporcionarse como "lspk/altavoz"
-        tmp = [x for x in args if "lspk/" in x]
+        # altavoz
+        tmp = [ x for x in args if "lspk/" in x ]
         if tmp:
             loudspeaker = tmp[0].split("lspk/")[1].replace("/", "") # por si se pasara con el slash final
 
-        # se pide ayuda
-        tmp = [x for x in args if "-h" in x]
-        if tmp or not bf_ini_file:
-            print __doc__
-            sys.exit(0)
+        # fs
+        tmp = [ x for x in args if x.isdigit() ]
+        if tmp:
+            fs = tmp[0]
 
     else:
-        print __doc__
+        print "(!) Indicar lspk/ALTAVOZ o -h para ayuda"
         sys.exit(0)
 
-    # Variables de trabajo (bien por defecto o bien las que se pasen como argumento)
+    # Carpeta de audio:
     audio_folder     = loudspeaker_folder + loudspeaker + "/" + fs
-    filters_ini_file = audio_folder + "/filters.ini"
+    if not os_path.isdir(audio_folder):
+        print "(!) NO existe '" + audio_folder + "'"
+        sys.exit()
 
-    # Cargamos con el contenido
-    bf_ini.read(bf_ini_file)
+    bf_ini_file      = loudspeaker_folder + loudspeaker + "/brutefir_config.ini"
 
-    # lee y verifica bf_ini
-    fs     = float(lee_secc_gral("fs"))
+    if os_path.exists(bf_ini_file):
+        bf_ini.read(bf_ini_file)
+    else:
+        print "(!) NO existe '" + bf_ini_file + "'"
+        sys.exit()
+
     dither = lee_secc_gral("dither")
 
     CANALES = []
@@ -418,13 +516,12 @@ if __name__ == "__main__":
     if viasCorrectas:
         avisos.append('\n(i) Se ha validado la configuracion de vías.')
 
-        # scanfilters genera filters.ini con los filtros encontrados en la carpeta del altavoz.
+        # scanfilters genera filters.scan con los filtros encontrados en la carpeta del altavoz.
         scanfilters.main(audio_folder)
-
-        # Cargamos con el contenido
+        filters_ini_file = audio_folder + "/filters.scan"
         filters_ini.read(filters_ini_file)
 
-        # Ahora generamos brutefir_config a partir de brutefir.ini y filters.ini
+        # Ahora generamos brutefir_config a partir de brutefir.ini y filters.scan
 
         # // redirigimos el printado a fichero si así se requiriera
         # memorizamos la stdout original (la consola)
@@ -444,7 +541,7 @@ if __name__ == "__main__":
         print   "(i) Atención:"
         if salidaPorConsola:
             print "\n(i) Se ha leido:", bf_ini_file
-            print "\n(i) Se ha leido:", loudspeaker_folder + loudspeaker + "/" + str(int(fs))
+            print "\n(i) Se han buscado pcms en:", loudspeaker_folder + loudspeaker + "/" + str(int(fs))
             print "\n(!) Usar la opción -w para guardar en:"
             print "\n        " + audio_folder + "/brutefir_config"
         else:
@@ -456,4 +553,3 @@ if __name__ == "__main__":
     for aviso in avisos:
         print aviso
     print "\nBye!"
-    print "\n(i) OjO VERSION EN PRUEBAS admite configuraciones FR + VIAS + SUB altogether\n"
